4ac58dab394a78e1bfff8b7ca7a3e798257ae25c
sachinkote <65725161+sachinkote@users.noreply.github.com>
2023-03-04 17:08:23 +0530

    Update UserServiceImpl.java

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
 import com.driver.model.Connection;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
 import com.driver.model.ServiceProvider;
 import com.driver.model.User;
 
@@ -9,6 +11,7 @@ import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
 import com.driver.services.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,20 +30,52 @@ public class UserServiceImpl implements UserService {
     @Override
     public User register(String username, String password, String countryName) throws Exception{
     	User user=new User();
-    	user.setConnected(false);
-    	List<Connection> l=null;
-    	user.setConnectionList(l);
-    	user.setCountry(null);
-    	user.setMaskedIp(null);
-    	//user.setOriginalIp(user.getCountry().getCountryName().toCode()+"."+user.getId());
     	user.setUsername(username);
     	user.setPassword(password);
-    	List<ServiceProvider> l2=null;
-    	user.setServiceProviderList(l2);
-    	return userRepository3.save(user);
+    	Country c=new Country();
     	
+    	String name=countryName.toUpperCase();
+    	if(name.equals("IND")) {
+    		c.setCountryName(CountryName.IND);
+    		c.setCode(CountryName.IND.toCode());
+    		user.setMaskedIp(""+CountryName.IND.toCode()+"."+user.getId());
+    	}
+    	else if(name.equals("AUS"))
+    	{
+    		c.setCountryName(CountryName.AUS);
+    		c.setCode(CountryName.AUS.toCode());
+    		user.setMaskedIp(""+CountryName.AUS.toCode()+"."+user.getId());
+    	}
+    	else if(name.equals("CHI"))
+    	{
+    		c.setCountryName(CountryName.CHI);
+    		c.setCode(CountryName.CHI.toCode());
+    		user.setMaskedIp(""+CountryName.CHI.toCode()+"."+user.getId());
+    	}
+    	else if(name.equals("JPN"))
+    	{
+    		c.setCountryName(CountryName.JPN);
+    		c.setCode(CountryName.JPN.toCode());
+    		user.setMaskedIp(""+CountryName.JPN.toCode()+"."+user.getId());
+    	}
+    	else if(name.equals("USA"))
+    	{
+    		c.setCountryName(CountryName.USA);
+    		c.setCode(CountryName.USA.toCode());
+    		user.setMaskedIp(""+CountryName.USA.toCode()+"."+user.getId());
+    	}
+    	c.setServiceProvider(null);
+    	user.setConnected(false);
+    	List<Connection>l=new ArrayList<>();
+    	user.setConnectionList(l);
+    	user.setMaskedIp(null);
+    	user.setOriginalCountry(name);
+		user.setCountry(c);
 		
-
+		//userRepository3.save(user);
+		c.setUser(user);
+		countryRepository3.save(c);
+       return user;
     }
 
     @Override
@@ -49,7 +84,7 @@ public class UserServiceImpl implements UserService {
 		List<ServiceProvider>l1=user.getServiceProviderList();
 		ServiceProvider sp=serviceProviderRepository3.findById(serviceProviderId).get();
 		List<User> l=sp.getUsers();
-		
+	    	
 		l.add(user);
 		sp.setUsers(l);
 		l1.add(sp);
